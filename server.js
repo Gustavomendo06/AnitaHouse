9121a20d0307fa1eeb2d12d63084f08c0bcf2a15
Gustavo Mendo <Gustavomendo22pt@gmail.com>
2025-06-09 10:57:25 +0100

    Usar better-sqlite3 e preparar para Render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const db = require('./db');
+const db = require('better-sqlite3');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
